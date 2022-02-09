3b22b2e13a06f2d69d28b8da202966edcf7edd11
Gihyeon KIM <ken29511@gmail.com>
2022-02-09 20:45:06 +0900

    UseEffect

---
+++ src/UseEffect.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+function App2() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time.");
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'conter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'conter' changes.");
+  }, [keyword, counter]);
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import UseEffect from "./UseEffect";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <UseEffect />
   </React.StrictMode>,
   document.getElementById("root")
 );
